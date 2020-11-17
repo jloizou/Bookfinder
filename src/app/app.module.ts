a45c1397d43ff0f3425cf376b56c7d4daaebd9df
James <james.loizou@nology.io>
2020-11-17 12:54:25 +0000

    working and styled passably

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { Routes, RouterModule} from '@angular/router';
 import { AppComponent } from './app.component';
+import { SearchComponent } from './search/search.component';
+import { CollectionComponent } from './collection/collection.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from "@angular/forms"
+
+
+
+const routes: Routes = [ 
+  {path: '', component: SearchComponent},
+  {path: 'collection', component: CollectionComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent, 
+    CollectionComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, 
+    RouterModule.forRoot(routes), 
+    HttpClientModule, 
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/search/search.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit, Output } from '@angular/core';
+import { from } from 'rxjs';
+import { HttpClient } from "@angular/common/http"
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.css']
+})
+export class SearchComponent implements OnInit {
+
+  searchTerm: string;
+  books: object;
+
+ 
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+  }
+
+
+
+  getBooks() { 
+    return this.http.get(`https://www.googleapis.com/books/v1/volumes?q=inauthor=${this.searchTerm}`).toPromise()
+    .then((response: object) => { 
+      this.books = response['items']
+      console.log(this.books)
+    })
+  }
+
+}
