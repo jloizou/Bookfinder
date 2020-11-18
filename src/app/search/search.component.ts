073f6d7ae05bf78ce38e9a367abf2d193065bb0c
James <james.loizou@nology.io>
2020-11-18 12:55:40 +0000

    just some more styling needed

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Output } from '@angular/core';
 import { from } from 'rxjs';
 import { HttpClient } from "@angular/common/http"
+import { AngularFirestore } from "@angular/fire/firestore"
 
 @Component({
   selector: 'app-search',
@@ -11,27 +12,34 @@ export class SearchComponent implements OnInit {
 
   searchTerm: string;
   books: object;
-
-  handleSave() { 
-    
-  }
-
- 
+  isFavOpen: boolean;
+  favBooks: any;
 
   constructor(private http: HttpClient, 
               private firestore: AngularFirestore
               ) { }
 
   ngOnInit(): void {
+    this.isFavOpen = false
+    this.firestore.collection('favourited').get().toPromise().then(response => {
+      this.favBooks = response.docs.map(d => d.data())
+    })
   }
 
+  handleSave(book) { 
+    this.firestore.doc(`favourited/${book.id}`).set(book , {merge: true})
+  }
+
+  handleFavButtonClick() { 
+    this.isFavOpen = !this.isFavOpen
+  }
 
 
   getBooks() { 
     return this.http.get(`https://www.googleapis.com/books/v1/volumes?q=inauthor=${this.searchTerm}`).toPromise()
     .then((response: object) => { 
       this.books = response['items']
-      console.log(this.books)
+      console.log(this.favBooks)
     })
   }
 
